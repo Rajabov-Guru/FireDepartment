12c939bf0035440071abb6d033a0ffdff2461f31
fergentp <shetnev.slava@mail.ru>
2022-03-23 13:17:33 +0300

    Заполнение классов Fireman, Guard, Travel

--- Model/Guard.cs
+++ Model/Guard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,12 @@ namespace FireDepartment.Model
     //Расчет
     public class Guard
     {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Senior { get; set; }
+        [Required]
+        public int Id_Complect { get; set; }
+        public virtual Travel Travel { get; set; }
     }
 }
--- Model/Travel.cs
+++ Model/Travel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,35 @@ namespace FireDepartment.Model
     //Путевка
     public class Travel
     {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public int Id_Guard { get; set; }
+        [Required]
+        public int Id_Act { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Patronymic { get; set; }
+        [Required]
+       
+        public int Telephone { get; set; }
+        [Required]
+        public DateTime Indate { get; set; }
+        [Required]
+        public string obj_ignition { get; set; }
+
+        [Required]
+        public string name_division { get; set; }
+
+        [Required]
+        public string type_ignition { get; set; }
+
+        public virtual ICollection<Guard> guards { get; set; }
+
+        public string FIO => $"{this.Surname} {this.Name} {this.Patronymic}";
+        public string IOF => $"{this.Name} {this.Patronymic} {this.Surname}";
     }
 }
