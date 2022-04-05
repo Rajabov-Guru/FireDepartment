64798e09e369431f3231bbeb5304155fde28eb06
AFROS <AFROS@DESKTOP-ETQHKOC>
2022-04-05 21:13:23 +0700

    Интерфейс: начало

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using FireDepartment.Classes;
 using FireDepartment.Model;
+using FireDepartment.Pages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,15 +26,17 @@ namespace FireDepartment
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Content = new MainPage();
 
-            //это код который инициирует создание БД (потом убрать)
-            //using (FireDB db = new FireDB())
-            //{
-            //    Console.WriteLine(db.Complects.Count());
-            //}
-
-            //ParseMethods.FillDB();
-            StaticMethods.Report();
+            
         }
     }
 }
+//это код который инициирует создание БД (потом убрать)
+//using (FireDB db = new FireDB())
+//{
+//    Console.WriteLine(db.Complects.Count());
+//}
+
+//ParseMethods.FillDB();
+//StaticMethods.Report();
