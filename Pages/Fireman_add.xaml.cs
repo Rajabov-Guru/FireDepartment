66bb19903ec09bee50c56b4ad6227597f79487dc
ферн <@DESKTOP-MM73S3P>
2022-04-23 16:19:14 +0300

    Добавление бойцов

--- Pages/Fireman_add.xaml.cs
+++ Pages/Fireman_add.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FireDepartment.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,46 @@ namespace FireDepartment.Pages
         public Fireman_add()
         {
             InitializeComponent();
+        }
+
+        private void Ok_Fa_Click(object sender, RoutedEventArgs e)
+        {
+            if (SurnameFireman.Text != ""&& NameFireman.Text != ""&& PatronymicFireman.Text != "")
+            {
+               
+                if (DateBirth.SelectedDate.HasValue )
+                {
+                    var data = DateTime.Parse(DateBirth.SelectedDate.Value.ToString("dd.MM.yyyy", System.Globalization.CultureInfo.InvariantCulture));
+                    if (data > DateTime.Parse("01.01.1980") && data < DateTime.Parse("01.2002"))
+                    {
+                        string formatted = DateBirth.SelectedDate.Value.ToString("dd.MM.yyyy", System.Globalization.CultureInfo.InvariantCulture);
+                        using (FireDB db = new FireDB())
+                        {
+                            Fireman fireman = new Fireman();
+                            fireman.Surname = SurnameFireman.Text;
+                            fireman.Name = NameFireman.Text;
+                            fireman.Patronymic = PatronymicFireman.Text;
+                            fireman.Date_birth = data;
+                            fireman.GuardId = db.Guards.Count()-1;
+                             db.Firemans.Add(fireman);
+                             db.SaveChanges();
+                            NavigationService.Navigate(new Fireman_list());
+                        }
+                    }
+                    else MessageBox.Show("Введите дату в промежутке (1980-2002 год)");
+                }
+               else MessageBox.Show("Введите дату");
+            }
+            else
+            {
+                MessageBox.Show("Введите данные");
+            }
+           
+        }
+
+        private void Cancel_Fa_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Fireman_list());
         }
     }
 }
