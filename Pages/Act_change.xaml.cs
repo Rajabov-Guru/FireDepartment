5b08a6ab5a27a53cd8295c463c3986bed1f48fd1
21Anton21 <roznin.a@mail.ru>
2022-04-24 15:46:46 +0700

    Акт Change

--- Pages/Act_change.xaml.cs
+++ Pages/Act_change.xaml.cs
@@ -71,16 +71,86 @@ namespace FireDepartment.Pages
 
         private void Ok_Ad_Click(object sender, RoutedEventArgs e)
         {
-            //проверки
-            using (FireDB db = new FireDB())
+            if (belongings.SelectedItem != null &&
+                Situation.Text != "" &&
+                FireEquipment.Text != "" &&
+                waterShit.SelectedItem != null &&
+                FireAutomatization.SelectedItem != null &&
+                Parished.Text != "" &&
+                ParishedChildren.Text != "" &&
+                ParishedEmployees.Text != "" &&
+                Injured.Text != "" &&
+                InjuredChildren.Text != "" &&
+                InjuredEmployees.Text != "" &&
+                DestoyedBuildings.Text != "" &&
+                DestoyedFlats.Text != "" &&
+                DestoyedFloorArea.Text != "" &&
+                DestoyedTechnicks.Text != "" &&
+                DestoyedAgricultural.Text != "" &&
+                DestoyedBeasts.Text != "" &&
+                DamagedBuildings.Text != "" &&
+                DamagedFlats.Text != "" &&
+                DamagedFloorArea.Text != "" &&
+                DamagedTechnicks.Text != "" &&
+                DamagedAgricultural.Text != "" &&
+                DamagedBeasts.Text != "" &&
+                Terms.Text != "" &&
+                Cause.Text != "" &&
+                RescuedHumans.Text != "" &&
+                RescuedTechnicks.Text != "" &&
+                RescuedBeasts.Text != "" &&
+                RescuedMaterialValues.Text != ""
+                ) // Проверка на целостность введенных данных
             {
-                Act act = db.Acts.Find(a.Id);
-                /*act.somthing=Somthing.Text;
-                ....
-                */
-                db.SaveChanges();
-                NavigationService.Navigate(new Act_list());
+                var data1 = DateTime.Parse(localization.SelectedDate.Value.ToString("dd.MM.yyyy", System.Globalization.CultureInfo.InvariantCulture));
+                var data2 = DateTime.Parse(Liquidation.SelectedDate.Value.ToString("dd.MM.yyyy", System.Globalization.CultureInfo.InvariantCulture));
+                if (data1 > DateTime.Parse("01.01.2022") && data1 < DateTime.Parse("31.12.2032") &&
+                    data2 > DateTime.Parse("01.01.2022") && data2 < DateTime.Parse("31.12.2032"))
+                {
+                    using (FireDB db = new FireDB())
+                    {
+                        Act act = db.Acts.Find(a.Id);
+                        act.Belonging = belongings.Text;
+                        act.Detection = data1;// Костыль
+                        act.Localization = data1;
+                        act.Liquidation = data2;
+                        act.Situation = Situation.Text;
+                        act.FireEquipment = FireEquipment.Text;
+                        act.WaterSupply = bool.Parse(waterShit.Text);
+                        act.AvailabilityOfFireAutomatic = bool.Parse(FireAutomatization.Text);
+                        act.Parished = int.Parse(Parished.Text);
+                        act.ParishedChildren = int.Parse(ParishedChildren.Text);
+                        act.ParishedEmployees = int.Parse(ParishedEmployees.Text);
+                        act.Injured = int.Parse(Injured.Text);
+                        act.InjuredChildren = int.Parse(InjuredChildren.Text);
+                        act.InjuredEmployees = int.Parse(InjuredEmployees.Text);
+                        act.DestoyedBuildings = int.Parse(DestoyedBuildings.Text);
+                        act.DestoyedFlats = int.Parse(DestoyedFlats.Text);
+                        act.DestoyedFloorArea = int.Parse(DestoyedFloorArea.Text);
+                        act.DestoyedTechnicks = int.Parse(DestoyedTechnicks.Text);
+                        act.DestoyedAgricultural = int.Parse(DestoyedAgricultural.Text);
+                        act.DestoyedBeasts = int.Parse(DestoyedBeasts.Text);
+                        act.DamagedBuildings = int.Parse(DamagedBuildings.Text);
+                        act.DamagedFlats = int.Parse(DamagedFlats.Text);
+                        act.DamagedFloorArea = int.Parse(DamagedFloorArea.Text);
+                        act.DamagedTechnicks = int.Parse(DamagedTechnicks.Text);
+                        act.DamagedAgricultural = int.Parse(DamagedAgricultural.Text);
+                        act.DamagedBeasts = int.Parse(DamagedBeasts.Text);
+                        act.Terms = Terms.Text;
+                        act.Cause = Cause.Text;
+                        act.RescuedHumans = int.Parse(RescuedHumans.Text);
+                        act.RescuedTechnicks = int.Parse(RescuedTechnicks.Text);
+                        act.RescuedBeasts = int.Parse(RescuedBeasts.Text);
+                        act.RescuedMaterialValues = int.Parse(RescuedMaterialValues.Text);
+                        db.SaveChanges();
+                        NavigationService.Navigate(new Act_list());
+                    }
+                }
+                else MessageBox.Show("Введите все даты в промежутке (2022-2032 год)");
+
             }
+            else MessageBox.Show("Введите данные");
+            
         }
 
         private void Sel_change(object sender, SelectionChangedEventArgs e)
